340bcedaf8639a1b3e4e954c5c870baf649dd873
mmarzouki <mail@mmarzouki.de>
2020-08-26 23:44:54 +0200

    verbesserte aussage beim einlesen der eingabe

--- ssl-tls_support_check.sh
+++ ssl-tls_support_check.sh
@@ -2,7 +2,7 @@
 # This script checks the encryption mechanism for a HTTPS site. I check the mechanism SSLv2 until TLSv1.3
 
 echo -e "This script checks the encryption mechanism of a webserver which is supported \nI check the encryption SSLv2 until TLSv1.3"
-read -p "Which HTTPS site do you want to check?: " URL
+read -p "Which HTTPS site do you want to check? Only Domain without https://[...]: " URL
 ENC="--sslv2 --sslv3 --tlsv1.1 --tlsv1.2 --tlsv1.3"
 
 for i in $ENC
